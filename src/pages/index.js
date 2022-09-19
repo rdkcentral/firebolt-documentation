c94510f042d4241f5f8319b3fe5092d46a9db3c0
ahorn-github <84027470+ahorn-github@users.noreply.github.com>
2022-09-19 08:21:01 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -516,9 +516,9 @@ Lifecycle.listen('foreground', value => {
         <Container className="textSection" background="light">
           <h2>Proudly Used Elsewhere</h2>
           <p>
-            Relay was originally created for the React Native sections of the
-            Facebook app, and it has been used adapted and improved by other
-            teams internally and externally.
+      //      Relay was originally created for the React Native sections of the
+      //      Facebook app, and it has been used adapted and improved by other
+      //      teams internally and externally.
           </p>
           <div>
             <div className="logosHomepage">{showcase}</div>

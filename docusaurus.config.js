30fb138a69d156e75dbaf003ab1e410974da361a
adam-horn5 <114593319+adam-horn5@users.noreply.github.com>
2022-10-03 10:53:51 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,12 +71,12 @@ module.exports = {
           //   internal: 'current',
           //   external: versions[0],
           // }),
-          includeCurrentVersion:false,
-     //     versions: {
-     //       current: {
-     //         label: 'Next 🚧',
-    //        },
-    //      },
+          includeCurrentVersion:true,
+          versions: {
+            current: {
+             label: 'Next 🚧',
+           },
+         },
 
         },
         blog: {},

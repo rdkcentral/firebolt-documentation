e3d8ca2c56326d9a09e76ea1a3e823a1afd7e65d
adam-horn5 <114593319+adam-horn5@users.noreply.github.com>
2022-10-19 09:29:53 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/firebolt-documentation/docs/firebolt_docs/Firebolt_Core_SDK/getting-started/quick-start-guide/' />;
+  return <Redirect to='/firebolt-documentation/docs/firebolt_docs/getting-started/quick-start-guide/' />;
 }
